6b4a317c7226ffa52de9945e774966aeebe1ac19
shixiaojun008 <42019609+shixiaojun008@users.noreply.github.com>
2021-06-08 18:16:09 +0800

    update

---
+++ src/Seasons.java
@@ -0,0 +1,13 @@
+public enum Seasons {
+    SPRING("1"),
+    SUMMER("2"),
+    AUTUMN("3"),
+    WINTER("4");
+
+    private String enumValue;
+
+    Seasons(String enumValue) {
+        this.enumValue = enumValue;
+    }
+
+}
